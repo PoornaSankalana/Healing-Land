fe1d1ed5f6d013030bfb091c5b2ab72d9333ada8
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-05 00:21:04 +0530

    visit event java

---
+++ app/src/main/java/com/scorpion/healingland/VisitEvent.java
@@ -0,0 +1,14 @@
+package com.scorpion.healingland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_event);
+    }
+}
