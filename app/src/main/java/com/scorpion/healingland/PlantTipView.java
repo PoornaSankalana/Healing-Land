d633618108953496a5687fa872bcd2b986471a62
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-05 01:53:37 +0530

    plant tip view java

---
+++ app/src/main/java/com/scorpion/healingland/PlantTipView.java
@@ -0,0 +1,13 @@
+package com.scorpion.healingland;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantTipView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_tip_view);
+    }
+}
