7d1840d109b1529c139db15fcf6b898bc0757e73
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-10 10:09:33 +0530

    Implementing garden tips table columns

--- app/src/main/java/com/scorpion/healingland/Database/DBHandler.java
+++ app/src/main/java/com/scorpion/healingland/Database/DBHandler.java
@@ -121,19 +121,20 @@ public class DBHandler extends SQLiteOpenHelper {
         }
     }
 
-    public long AddGardenTips (String plantName, String botanicalName, String plantType, String water, String plantingTip, String fertilizerTip, String imageUrl){
+    public long AddGardenTips (String plantCode, String plantName, String botanicalName, String plantType, String water, String plantingTip, String fertilizerTip, String imageUrl){
         // Get the database instance in write mode
         SQLiteDatabase db = getWritableDatabase();
 
         // Create a new map of values, where column names are the keys
         ContentValues contentValues = new ContentValues();
-        contentValues.put(Fields.GardenTipsData.COLUMN_1, plantName);
-        contentValues.put(Fields.GardenTipsData.COLUMN_2, botanicalName);
-        contentValues.put(Fields.GardenTipsData.COLUMN_3, plantType);
-        contentValues.put(Fields.GardenTipsData.COLUMN_4, water);
-        contentValues.put(Fields.GardenTipsData.COLUMN_5, plantingTip);
-        contentValues.put(Fields.GardenTipsData.COLUMN_6, fertilizerTip);
-        contentValues.put(Fields.GardenTipsData.COLUMN_7, imageUrl);
+        contentValues.put(Fields.GardenTipsData.COLUMN_1, plantCode);
+        contentValues.put(Fields.GardenTipsData.COLUMN_2, plantName);
+        contentValues.put(Fields.GardenTipsData.COLUMN_3, botanicalName);
+        contentValues.put(Fields.GardenTipsData.COLUMN_4, plantType);
+        contentValues.put(Fields.GardenTipsData.COLUMN_5, water);
+        contentValues.put(Fields.GardenTipsData.COLUMN_6, plantingTip);
+        contentValues.put(Fields.GardenTipsData.COLUMN_7, fertilizerTip);
+        contentValues.put(Fields.GardenTipsData.COLUMN_8, imageUrl);
 
         // insert the new row and returning
         long newRow = db.insert(Fields.GardenTipsData.TABLE_NAME, null, contentValues);
