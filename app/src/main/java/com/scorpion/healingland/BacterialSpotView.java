6bad38c3e43f47bb95ba5aad681fa76f808075fb
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-11-13 00:20:55 +0530

    bacterial spot view backend activity

--- app/src/main/java/com/scorpion/healingland/BacterialSpotView.java
+++ app/src/main/java/com/scorpion/healingland/BacterialSpotView.java
@@ -1,18 +1,81 @@
 package com.scorpion.healingland;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class BacterialSpotView extends Fragment {
+    Button update, delete;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bacterial_spot_view, container,false);
+        View view =  inflater.inflate(R.layout.fragment_bacterial_spot_view, container,false);
+
+        update = view.findViewById(R.id.updateDisease);
+        delete = view.findViewById(R.id.deleteDisease);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
+            @Override
+            public void onClick(View view) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+                builder.setTitle("Delete Confirmation");
+                builder.setBackground(getResources().getDrawable(R.drawable.alert_dialog_background));
+                builder.setIcon(R.drawable.ic_round_warning_24);
+                builder.setMessage("Are you sure want to delete this?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getActivity(), "Successfully Deleted!", Toast.LENGTH_SHORT).show();
+                                PlantDiseases plantDiseases = new PlantDiseases();
+                                FragmentManager fragmentManager = getFragmentManager();
+                                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.replace(R.id.fragment_container, plantDiseases);
+                                fragmentTransaction.addToBackStack(null);
+                                fragmentTransaction.commit();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditDiseases editDiseases = new EditDiseases();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, editDiseases);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
