1e2d0c103ce8f7183c448ec2f0719ead816fa187
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-11-13 00:21:14 +0530

    edit diseases backend activity

--- app/src/main/java/com/scorpion/healingland/EditDiseases.java
+++ app/src/main/java/com/scorpion/healingland/EditDiseases.java
@@ -1,14 +1,46 @@
 package com.scorpion.healingland;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class EditDiseases extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class EditDiseases extends Fragment {
+    Button update, delete;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_diseases);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_diseases, container, false);
+
+        update = view.findViewById(R.id.uDisease);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Updated Successfully!", Toast.LENGTH_SHORT).show();
+                PlantDiseases plantDiseases = new PlantDiseases();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, plantDiseases);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
