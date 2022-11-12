2e1da2a00baf28c23e58ae9bf6b3510a699cbd03
Udul-D <uduldulsara@gmail.com>
2022-11-12 11:00:20 +0530

    Implement plant add details java file

---
+++ app/src/main/java/com/scorpion/healingland/PlantAdd.java
@@ -0,0 +1,14 @@
+package com.scorpion.healingland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlantAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_add);
+    }
+}
